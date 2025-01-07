e360f80995997b0d8be63576c53d2e8dd98cf4dc
SagarSRaut <sagar1811raut@gmail.com>
2025-01-07 23:53:07 +0530

    Bubble_sort Algorithm

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+class Bubble:
+    
+    def bubble_sort(self,num):
+        n=len(num)
+        for i in range(n):
+            for j in range(n-1):
+                if num[j]>num[j+1]:
+                    temp=num[j]
+                    num[j]=num[j+1]
+                    num[j+1]=temp
+        return num
+
+
+b=Bubble
+v=[40,70,30,80,100]
+print(b.bubble_sort(v))
